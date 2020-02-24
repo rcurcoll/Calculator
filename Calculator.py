252494975feaba1ac928b9cc3f5a12463c7fded9
rcurcoll <rcurcoll@gmail.com>
2020-02-24 11:51:53 +0100

    first commit

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 11:31:06 2020
+
+@author: rcurcoll
+"""
+class Calculator:
+    
+    urbag_constant = 12.4
+    
+    def sum(a,b):
+        return a+b
+    
---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 11:37:22 2020
+
+@author: rcurcoll
+"""
+from Calculator import Calculator
+
+result = Calculator.sum(3,4)
+
+print("El resultat de la suma és",result)
